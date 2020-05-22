f93dbf982bd145f85f875f29f7910765e28f5bad
normandesjuniorzup <normandes.junior@zup.com.br>
2020-05-22 14:48:19 -0300

    Add ConsumerRecord to see key and value

--- src/main/java/com/hibicode/kafka/consumer/RechargeConsumer.java
+++ src/main/java/com/hibicode/kafka/consumer/RechargeConsumer.java
@@ -1,9 +1,11 @@
 package com.hibicode.kafka.consumer;
 
 import com.hibicode.kafka.consumer.model.RechargeRequest;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +14,8 @@ public class RechargeConsumer {
     private static final Logger log = LoggerFactory.getLogger(RechargeConsumer.class);
 
     @KafkaListener(topics = "test1", containerFactory = "kafkaJsonListenerContainerFactory")
-    public void process(RechargeRequest msg) {
-        log.info("Msg: " + msg);
+    public void process(@Payload ConsumerRecord<String, RechargeRequest> consumerRecord) {
+        log.info("Msg: " + consumerRecord.value() + " - key: " + consumerRecord.key());
     }
 
 }
