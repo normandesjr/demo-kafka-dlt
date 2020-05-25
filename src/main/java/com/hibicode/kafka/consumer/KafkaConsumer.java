4681bb9e4b23216b00c7bab8dcda4803b9b5268c
normandesjuniorzup <normandes.junior@zup.com.br>
2020-05-25 15:30:10 -0300

    Add stop at index

--- src/main/java/com/hibicode/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/hibicode/kafka/consumer/KafkaConsumer.java
@@ -8,17 +8,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.kafka.support.KafkaHeaders.RECEIVED_TOPIC;
 
 @Service
 public class KafkaConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    private void process(ConsumerRecord<String, FormRequest> consumerRecord) {
+    private void process(ConsumerRecord<String, FormRequest> consumerRecord, String receivedTopic) {
         var formRequest = consumerRecord.value();
 
+        log.info("received topic - {}", receivedTopic);
+
+        if (!shouldStopThrowError(receivedTopic, formRequest)) {
+            throwSomeException(formRequest);
+        }
+    }
+
+    private void throwSomeException(FormRequest formRequest) {
         switch (formRequest.getErrorType()) {
             case RETRYABLE:
                 throw new RetryableException("Retryable exception");
@@ -27,14 +38,21 @@ public class KafkaConsumer {
         }
     }
 
+    private boolean shouldStopThrowError(String receivedTopic, FormRequest formRequest) {
+        var retryIndex = receivedTopic.replaceAll("[^\\d]", "");
+        return formRequest.getRetryableStopAt().getIndex().equals(retryIndex);
+    }
+
     @KafkaListener(
             id = "main-topic-listener",
             topics = "${app.kafka.topic}",
             containerFactory = "mainKafkaListenerContainerFactory")
-    public void process(@Payload ConsumerRecord<String, FormRequest> consumerRecord, Acknowledgment ack) {
+    public void process(@Payload ConsumerRecord<String, FormRequest> consumerRecord,
+                        Acknowledgment ack,
+                        @Header(name = RECEIVED_TOPIC) String receivedTopic) {
         try {
             log.info("Start main consumer");
-            process(consumerRecord);
+            process(consumerRecord, receivedTopic);
         } finally {
             ack.acknowledge();
         }
@@ -49,10 +67,12 @@ public class KafkaConsumer {
                     "fetch.max.wait.ms=${app.kafka.dlt.retry.max.wait.ms}"
             }
     )
-    public void retry(@Payload ConsumerRecord<String, FormRequest> consumerRecord, Acknowledgment ack) throws Exception {
+    public void retry(@Payload ConsumerRecord<String, FormRequest> consumerRecord,
+                      Acknowledgment ack,
+                      @Header(name = RECEIVED_TOPIC) String receivedTopic) {
         try {
             log.info("Start retry");
-            process(consumerRecord);
+            process(consumerRecord, receivedTopic);
         } finally {
             ack.acknowledge();
         }
--- src/main/java/com/hibicode/kafka/model/dto/RetryableStopAt.java
+++ src/main/java/com/hibicode/kafka/model/dto/RetryableStopAt.java
@@ -2,8 +2,18 @@ package com.hibicode.kafka.model.dto;
 
 public enum RetryableStopAt {
 
-    DLT,
-    RETRY_1,
-    RETRY_2,
-    RETRY_3
+    DLT("0"),
+    RETRY_1("1"),
+    RETRY_2("2"),
+    RETRY_3("3");
+
+    private String index;
+
+    RetryableStopAt(String index) {
+        this.index = index;
+    }
+
+    public String getIndex() {
+        return index;
+    }
 }
